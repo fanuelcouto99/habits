c6b3a5edb49de998c0059e98b2dfc05f0e08d179
Fanuel Couto <fanuel.couto99@gmail.com>
2023-01-22 19:21:52 -0300

    Configurado key para o map

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -28,15 +28,15 @@ export function SummaryTable() {
                 {
                     summaryDates.map(date => {
                         return (
-                            <HabitDay key={date.toString()}/>
+                            <HabitDay key={date.toString()} />
                         );
                     })
                 }
-                
+
                 {/* Exibindo os quadrados na tela para não ficar vazia a tela */}
-                {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map(() => {
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                     return (
-                        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+                        <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
                     )
                 })}
             </div>
