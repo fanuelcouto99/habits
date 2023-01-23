0e1135229186f29c26a9dc849d650a11640fad50
Fanuel Couto <fanuel.couto99@gmail.com>
2023-01-22 21:18:33 -0300

    Criando home com a tabela de habitos

---
+++ mobile/src/components/HabitDay.tsx
@@ -0,0 +1,19 @@
+import { TouchableOpacity, Dimensions } from 'react-native';
+
+// Definido quantos quadrados irão aparecer por linha
+const WEEK_DAYS = 7;
+// Definindo o tamanho disponivel na tela para exibir os quadrados
+const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
+// Definindo o espaçamento entre os quadrados
+export const DAY_MARGIN_BETWEEN = 8;
+// Definindo o tamanho que cada quadrado vai ter
+export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5);
+
+export function HabitDay() {
+    return (
+        <TouchableOpacity
+            className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800'
+            style={{ width: DAY_SIZE, height: DAY_SIZE }}
+        />
+    );
+};
