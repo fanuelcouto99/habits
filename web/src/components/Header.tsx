a3ad6dec84d91d63e79187ab91bb98184e8ffb96
Fanuel Couto <fanuel.couto99@gmail.com>
2023-01-23 21:34:13 -0300

    Iniciando configuração do modal

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,12 +1,23 @@
 import { Plus } from 'phosphor-react';
+import { useState } from 'react';
 import logoImage from '../assets/logo.svg';
 
 export function Header() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    function buttonClicked() {
+        setIsModalOpen(true);
+    };
+
     return (
         <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
             <img src={logoImage} alt='Habits' />
 
-            <button type='button' className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'>
+            {
+                isModalOpen ? (): ()
+            }
+
+            <button type='button' onClick={buttonClicked} className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'>
                 <Plus size={20} className='text-violet-500' />
                 Novo Hábito
             </button>
