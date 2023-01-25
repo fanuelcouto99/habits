e7ba906fd0baea4c070eeed7cea9ffc5de3ccd3c
Fanuel Couto <fanuel.couto99@gmail.com>
2023-01-24 21:10:23 -0300

    Criando popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,22 @@
+import * as Popover from '@radix-ui/react-popover';
+
 export function HabitDay() {
     return (
-        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+        <Popover.Root>
+            <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+            <Popover.Portal>
+                <Popover.Content className='min-w-[320px] w-full p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+                    <span className='font-semibold text-zinc-400'>Segunda-feira</span>
+                    <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+
+                    <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+                        <div className='h-3 rounded-lg bg-violet-600 w-3/4' />
+                    </div>
+
+                    <Popover.Arrow className='fill-zinc-900' height={8} width={16}/>
+                </Popover.Content>
+            </Popover.Portal>
+        </Popover.Root>
     );
 };
