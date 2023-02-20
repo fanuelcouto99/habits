293ea3c2740dc0d1d0d4f425432624e5c748c131
Fanuel Couto <fanuel.couto99@gmail.com>
2023-02-20 15:22:11 -0300

    Corrigido importação do map, ao buscar dados no servidor

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,10 @@
+import { useEffect, useState } from "react";
 import { View, Text, ScrollView, Alert } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 import { api } from "../lib/axios";
-import { useEffect, useState } from "react";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearStart = generateRangeDatesFromYearStart();
@@ -20,11 +20,11 @@ export function Home() {
         try {
             setLoading(true);
             const response = await api.get('/summary');
-            console.log(response.data)
+            console.log('Teste: ', summary)
             setSummary(response.data);
         } catch (error) {
             Alert.alert('Ops!', 'Não foi possível carregar os sumário de hábitos!');
-            console.log(error);
+            console.log(JSON.stringify(error));
         } finally {
             setLoading(false);
         }
@@ -40,9 +40,9 @@ export function Home() {
 
             <View className="flex-row mt-6 mb-2">
                 {
-                    weekDays.map((weekDay, index) => {
+                    weekDays.map((weekDay, i) => {
                         return (
-                            <Text key={`${weekDay}-${index}`} className="text-zinc-400 text-xl font-bold text-center mx-1" style={{ width: DAY_SIZE }}>
+                            <Text key={`${weekDay}-${i}`} className="text-zinc-400 text-xl font-bold text-center mx-1" style={{ width: DAY_SIZE }}>
                                 {weekDay}
                             </Text>
                         )
@@ -52,7 +52,7 @@ export function Home() {
             <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 100 }}>
                 <View className="flex-row flex-wrap">
                     {
-                        datesFromYearStart.map(date => {
+                        datesFromYearStart.map((date, i) => {
                             return (
                                 <HabitDay key={date.toISOString()}
                                     onPress={() => navigate('habit', { date: date.toISOString() })}
