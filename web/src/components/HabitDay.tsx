8498ee23b79ce042877ba1d6c056907051831b7b
Fanuel Couto <fanuel.couto99@gmail.com>
2023-02-21 15:43:15 -0300

    Configurado focus nos componentes e finalizando aplicação web

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -29,7 +29,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
                 e no segundo quais as chaves, ou seja, para o bg-violet-500 só vai ser aplicado se o percentual foi maior ou 
                 igual a 80&
             */}
-            <Popover.Trigger className={clsx('w-10 h-10 border-2 rounded-lg transition-colors', {
+            <Popover.Trigger className={clsx('w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background', {
                 'bg-zinc-900 border-zinc-800': completedPercentage === 0,
                 'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
                 'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
