a723150642ed672a975358b4af84c48b5db6e160
Fanuel Couto <fanuel.couto99@gmail.com>
2023-02-21 14:59:02 -0300

    Criado função para marcar ou desmarcar checkbox alterando no banco de dados

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -2,6 +2,7 @@ import * as Checkbox from "@radix-ui/react-checkbox";
 import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
 import { Check } from "phosphor-react";
+import dayjs from "dayjs";
 
 interface HabitsListProps {
     date: Date;
@@ -29,6 +30,28 @@ export function HabitsList({ date }: HabitsListProps) {
         })
     }, []);
 
+    const isDateInPast = dayjs(date).endOf('day').isBefore(new Date());
+
+    async function handleToggleHabit(habitId: string) {
+        await api.patch(`/habits/${habitId}/toggle`);
+
+        const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
+
+        let completedHabits: string[] = [];
+
+        if (isHabitAlreadyCompleted) {
+            completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
+        } else {
+            completedHabits = [...habitsInfo!.completedHabits, habitId]
+        }
+
+        setHabitsInfo({
+            possibleHabits: habitsInfo!.possibleHabits,
+            completedHabits
+        });
+
+    };
+
     return (
         <div className="mt-6 flex flex-col gap-3">
             {habitsInfo?.possibleHabits.map(habit => {
@@ -36,7 +59,9 @@ export function HabitsList({ date }: HabitsListProps) {
                     <Checkbox.Root
                         key={habit.id}
                         className='flex items-center gap-3 group'
+                        onCheckedChange={() => handleToggleHabit(habit.id)}
                         checked={habitsInfo.completedHabits.includes(habit.id)}
+                        disabled={isDateInPast}
                     >
                         <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
                             <Checkbox.Indicator>
