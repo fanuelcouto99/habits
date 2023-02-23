7dcbe9bb89723ecc203d1b4ae93952bc27aab88c
Fanuel Couto <fanuel.couto99@gmail.com>
2023-02-23 10:16:13 -0300

    Retirado instalação do express e configurado novamente o fastify para não dar erro de conexão com mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,15 @@
-// import express from 'express';
-// import cors from 'cors';
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { appRoutes } from './routes';
 
-// const app = express();
 const app = Fastify();
 
-// app.use(cors());
 app.register(cors, {
     origin: true
 });
 
-// app.use(express.json());
 app.register(appRoutes);
 
-// app.use(appRoutes);
-
-// app.listen(3333, () => {
-//     console.log('HTTP: Server Running!');
-// });
 app.listen({
     port: 3333,
     host: '0.0.0.0'
